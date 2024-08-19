b7b088861c31caa1803c104d95fad81a5c7c5c74
VipulAlgoSoul <iamengineeringaddict@gmail.com>
2024-08-19 22:22:01 +0530

    Fixed the bug

--- new_containment.py
+++ new_containment.py
@@ -17,35 +17,38 @@ def get_score(predinp, gtinp):
 
 
     slope_index=[]
-    metrix = {i:[lvdis(prd,grd) for grd in gt_tu] for i,prd in enumerate(pred_tu)}
+    metrix = {i:[prd,[lvdis(prd,grd) for grd in gt_tu]] for i,prd in enumerate(pred_tu)}
 
-    for ind, prd in metrix.items():
+    for ind, prdd in metrix.items():
+        prd= prdd[1]
         minval = min(prd)
-        if prd.count(minval)>1:
-            slope_index.append(tuple([inx for inx,v in enumerate(prd) if v==minval]))
-        else:
-            slope_index.append(prd.index(minval))
+        if minval < len(prdd[0])/4:
+            if prd.count(minval)>1:
+                slope_index.append(tuple([inx for inx,v in enumerate(prd) if v==minval]))
+            else:
+                slope_index.append(prd.index(minval))
 
     master_path={"0":[]}
-    for inddx in slope_index:
+    try:
+        for inddx in slope_index:
 
-        if isinstance(inddx,tuple):
-            ini_dic={}
-            for indl in inddx:
-                for k in master_path.keys():
-                    ini_dic[str(indl)+str(k)] = master_path[k].copy()
-                    ini_dic[str(indl)+str(k)].append(indl)
-            master_path =ini_dic
+            if isinstance(inddx,tuple):
+                ini_dic={}
+                for indl in inddx:
+                    for k in master_path.keys():
+                        ini_dic[str(indl)+str(k)] = master_path[k].copy()
+                        ini_dic[str(indl)+str(k)].append(indl)
+                master_path =ini_dic
 
-        else:
-            for kyi in master_path.keys():
-                master_path[kyi].append(inddx)
+            else:
+                for kyi in master_path.keys():
+                    master_path[kyi].append(inddx)
 
 
-    slope_consistency = {k:len(set([i-v[e+1] for e,i in enumerate(v[0:-1])])) for k,v in master_path.items()}
-    minkey = min(slope_consistency, key=slope_consistency.get)
-    score = len(master_path[minkey])/len(gt_tu)
-
+        slope_consistency = {k:len(set([i-v[e+1] for e,i in enumerate(v[0:-1])])) for k,v in master_path.items()}
+        minkey = min(slope_consistency, key=slope_consistency.get)
+        score = len(master_path[minkey])/len(gt_tu)
+    except: score =0
 
     return score
 
@@ -56,4 +59,4 @@ def Contain_Score(pred,gt):
         score1 = get_score(pred=gt,gt=pred)
     return score1
 
-print(Contain_Score(pred="i am vipulnath,hi how are you ", gt="i am vipulnath, hi how are you"))
+print(Contain_Score(pred="hi how are you ", gt="where is are you"))
